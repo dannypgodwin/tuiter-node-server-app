3ab7adaab129630844614850025dc4fa6ed19a80
Danny Godwin <63467988+dannypgodwin@users.noreply.github.com>
2023-04-04 21:50:15 -0400

    Test.

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -1,21 +1,21 @@
 export default [
     {
      "_id": "123", "topic": "Space", "username": "SpaceX",
-     "handle": "@spacex", "time": "2h", "avatar_img": "../imgs/spacex.jpg",
+     "handle": "@spacex", "time": "2h", "image": "../imgs/spacex.jpg",
      "title": "SpaceX's Mission",
      "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
      "liked": true, "likes": 1234, "replies": 243, "retuits": 631
     },
     {
      "_id": "234", "topic": "Traffic", "username": "The Boring Company",
-     "handle": "@boringcompany", "time": "2h", "avatar_img": "../imgs/boring2.jpg",
+     "handle": "@boringcompany", "time": "2h", "image": "../imgs/nasa.jpg",
      "title": "The Boring Company fixing traffic",
      "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
      "liked": true, "likes": 342, "replies": 432, "retuits": 432
     },
     {
      "_id": "345", "topic": "Brain Injuries", "username": "Neurolink",
-     "handle": "@neurolink", "time": "3h", "avatar_img": "../imgs/neurolink.jpg",
+     "handle": "@neurolink", "time": "3h", "image": "../imgs/tesla.jpg",
      "title": "Neurolink fixes paralisis and blindness",
      "tuit": "Neurolink is an American company founded by Elon Musk that specializes in brain to computer integration. The first application will be to cure spinal cord injuries so that paraplegics can walk again. Other applications include curing blindness and mental disorders.",
      "liked": true, "likes": 3456, "replies": 432, "retuits": 654
