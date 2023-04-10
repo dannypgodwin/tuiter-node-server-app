04bee61a82ad99d284628c37f3836249adb68f94
Danny Godwin <63467988+dannypgodwin@users.noreply.github.com>
2023-04-09 21:23:09 -0400

    Committing A9.

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
-mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
+const CONNECTION_STRING ="mongodb+srv://dannypgodwin:mysupersecretpassword@cluster0.w61fcan.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(cors());
