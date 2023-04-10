a1d9bee05a80e81d9c73a24ecbdbff80a4abe48a
Danny Godwin <63467988+dannypgodwin@users.noreply.github.com>
2023-04-09 20:35:44 -0400

    Committing A9.

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,40 +1,38 @@
-import posts from "./tuits.js";
-let tuits = posts;
+// import posts from "./tuits.js";
+// let tuits = posts;
 
-const createTuit = (req, res) => {
+import * as tuitsDao from "./tuits-dao.js";
+
+const createTuit = async (req, res) => {
     const newTuit = req.body;
-    newTuit._id = (new Date()).getTime()+'';
     newTuit.likes = 0;
-    newTuit.replies = 0;
-    newTuit.retuits = 0;
     newTuit.liked = false;
-    newTuit.image = "spacex.jpg"
-    newTuit.handle = "@spacex"
-    tuits.push(newTuit);
-    res.json(newTuit);
-  }
-  
-const findTuits = (req, res) => res.json(tuits);
+    const insertedTuit = await tuitsDao
+        .createTuit(newTuit);
+    res.json(insertedTuit);
+}
 
-const updateTuit = (req, res) => {
+const findTuits = async (req, res) => {
+    const tuits = await tuitsDao.findTuits()
+    res.json(tuits);
+}
+const updateTuit = async (req, res) => {
     const tuitdIdToUpdate = req.params.tid;
     const updates = req.body;
-    const tuitIndex = tuits.findIndex(
-      (t) => t._id === tuitdIdToUpdate)
-    tuits[tuitIndex] = 
-      {...tuits[tuitIndex], ...updates};
-    res.sendStatus(200);
-  }
-  
-const deleteTuit = (req, res) => {
-    const tuitdIdToDelete = req.params.tid;
-    tuits = tuits.filter((t) =>
-      t._id !== tuitdIdToDelete);
-    res.sendStatus(200);
-  }
-  
+    const status = await tuitsDao
+        .updateTuit(tuitdIdToUpdate,
+            updates);
+    res.json(status);
+}
 
-export default (app) => {
+const deleteTuit = async (req, res) => {
+    const tuitdIdToDelete = req.params.tid;
+    const status = await tuitsDao
+        .deleteTuit(tuitdIdToDelete);
+    res.json(status);
+}
+
+    export default (app) => {
  app.post('/api/tuits', createTuit);
  app.get('/api/tuits', findTuits);
  app.put('/api/tuits/:tid', updateTuit);
