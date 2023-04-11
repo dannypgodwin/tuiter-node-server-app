28922ce94aeceba0d203ec64553bbca4091dd7ae
Danny Godwin <63467988+dannypgodwin@users.noreply.github.com>
2023-04-10 21:27:17 -0400

    Committing A9.

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,16 +1,18 @@
 import mongoose from 'mongoose';
 
 const schema = mongoose.Schema({
-    tuit: String,
-    likes: Number,
-    liked: Boolean,
-}, {collection: 'tuits'});
+        topic: String,
+        username: String,
+        handle: String,
+        time: String,
+        image: String,
+        title: String,
+        tuit: String,
+        likes: Number,
+        liked: Boolean,
+        replies: Number,
+        retuits: Number,
+        dislikes: Number,
+    }, {collection: 'tuits'}
+);
 export default schema;
-// load the mongoose library
-// create the schema
-// tuit property of type String
-// likes property of type Number
-// liked property of type Boolean
-// collection name where tuits are stored in tuiter database
-// export schema so it can be used elsewhere
-
